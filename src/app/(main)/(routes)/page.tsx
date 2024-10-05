86e9911647e82a9a3fbe9d394bca60986ce01139
Nishant Rai <n.rai.code@outlook.com>
2024-10-05 15:18:39 +0530

    added clerk(Authentication) and added middleware.ts in src

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+
+import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+
+
+
+  return (
+
+    <div >
+      <UserButton />
+    </div >
+  );
+}
