5343ec7c6fd622512c389ea03571cdd4d302cabd
Nishant Rai <n.rai.code@outlook.com>
2024-10-07 19:10:30 +0530

    added the invite modal

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -6,6 +6,7 @@ import { MemberRole } from '@prisma/client'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
 import { ChevronDown, PlusCircle, Settings, Trash, UserPlus, Users, LogOut } from 'lucide-react';
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { useModal } from '@/hooks/use-modal-store';
 
 
 interface ServerHeaderProps {
@@ -17,6 +18,7 @@ interface ServerHeaderProps {
 
 function ServerHeader({ server, role }: ServerHeaderProps) {
 
+    const { onOpen } = useModal();
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -32,7 +34,7 @@ function ServerHeader({ server, role }: ServerHeaderProps) {
             <DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]'>
                 {
                     isModerator && (
-                        <DropdownMenuItem className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer flex items-center'>
+                        <DropdownMenuItem onClick={() => onOpen("invite", { server })} className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer flex items-center'>
                             Invite People
                             <UserPlus className='h-4 w-4 ml-auto' />
                         </DropdownMenuItem>
