fb4f716be4e5b824471acd2f4c1404ed92b14caf
Nishant Rai <n.rai.code@outlook.com>
2024-10-11 18:34:48 +0530

    channelId page and memberId page added

--- src/app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,47 @@
+import ChatHeader from '@/components/chat/chat-header';
+import { currentProfile } from '@/lib/current-profile'
+import prisma from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-function ChannelId() {
+
+interface ChannelIdProps {
+    params: {
+        serverId: string,
+        channelId: string,
+    }
+}
+
+async function ChannelId({ params }: ChannelIdProps) {
+
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return auth().redirectToSignIn();
+    }
+
+    const channel = await prisma.channel.findUnique({
+        where: {
+            id: params?.channelId
+        }
+    });
+
+    const member = await prisma.member.findFirst({
+        where: {
+            sreverId: params.serverId,
+            profileId: profile.id
+        }
+    });
+
+    if (!channel || !member) {
+        return redirect("/");
+    }
+
     return (
-        <div>page</div>
+        <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+            <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+        </div>
     )
 }
 
--- src/app/(main)/(routes)/server/[serverId]/page.tsx
+++ src/app/(main)/(routes)/server/[serverId]/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation';
 
 interface ServerIdPageProps {
     params: {
-        serverId: string
+        serverId: string,
     }
 };
 
