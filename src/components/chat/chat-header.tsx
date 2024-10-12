8caa895acd1423804f78906eb9e986454ba03ea3
Nishant Rai <n.rai.code@outlook.com>
2024-10-12 15:47:49 +0530

    conversations.ts added that contains getOrCreateConversation() also added the memberId page or the conversation page

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react'
 import React from 'react'
 import { MobileToggle } from '../mobile-toggle'
+import UserAvatar from '../user-avatar'
 
 interface ChatHeaderProps {
     serverId: string,
@@ -16,6 +17,11 @@ function ChatHeader({ serverId, name, type, imageUrl }: ChatHeaderProps) {
             {
                 type === "channel" && (
                     <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />
+                )
+            }
+            {
+                type === "conversation" && (
+                    <UserAvatar src={imageUrl} className='h-8 w-8 md:w-8 md:h-8 mr-2' />
                 )
             }
 
