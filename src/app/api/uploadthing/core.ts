06e643e6972b0c3870f8dee9fcbf958c9a8d4f52
Nishant Rai <n.rai.code@outlook.com>
2024-10-16 22:21:18 +0530

    bug fix 3

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -15,10 +15,22 @@ const handleAuth = () => {
 export const ourFileRouter = {
     serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
         .middleware(() => handleAuth())
-        .onUploadComplete(() => { }),
+        .onUploadComplete(async ({ metadata, file }) => {
+            // This code RUNS ON YOUR SERVER after upload
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("file url", file.url);
+            // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+            return { value: file.url }
+        }),
     messageFile: f(["image", "pdf"],)
         .middleware(() => handleAuth())
-        .onUploadComplete(() => { })
+        .onUploadComplete(async ({ metadata, file }) => {
+            // This code RUNS ON YOUR SERVER after upload
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("file url", file.url);
+            // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+            return { value: file.url }
+        })
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
